1c33447f9631c7c0a71517cad236fb701b16b947
Samuele Perticarari <samuele.perticarari@gmail.com>
2023-02-14 01:19:40 +0100

    Removed commented and unused code

--- src/popup/components/popup.tsx
+++ src/popup/components/popup.tsx
@@ -1,39 +1,17 @@
 import React, { FC, useState } from "react";
 import { fetchKrunkerGames } from "../krunker-service";
-import './popup.scss'
+import "./popup.scss";
 
 const Popup = () => {
     //GLOBAL VARIABLES
     const [availablePlayersCount, setAvailablePlayersCount] = useState(4);
     const [games, setGames] = useState([] as any[]);
 
-    let mapSelection: string;
     let location: string = "de-fra";
-    let totPlayers: number;
-    const freePlayers: HTMLElement | null = document.getElementById("counter");
     const minInGamePlayers = 2;
     let custom: boolean = false;
     let url = "https://krunker.io/?game=";
-    let gamesArray = [];
     const audio: HTMLAudioElement = new Audio("/sounds/hover_0.mp3");
-
-    function createAnchor(title: string, url: string) {
-        // Create anchor element.
-
-        // Create the text node for anchor element.
-        const linkAnchor: HTMLAnchorElement = document.createElement("a");
-        // Append the text node to anchor element.
-        const linkTitle: Text = document.createTextNode(title);
-
-        // Set the title.
-        linkAnchor.appendChild(linkTitle);
-
-        // Set the properties.
-        linkAnchor.title = title;
-        linkAnchor.href = url;
-
-        return linkAnchor;
-    }
 
     //CORE FUNCTION: calls the API, filters data and produces output links
     function handleClickGetLinks(
@@ -42,6 +20,7 @@ const Popup = () => {
         fetchKrunkerGames()
             .then((response) => response.json())
             .then((data) => {
+                let gamesArray = [];
                 gamesArray = data.games.filter((game: any) => {
                     return (
                         game[1] == location &&
@@ -54,36 +33,6 @@ const Popup = () => {
                 gamesArray.sort();
 
                 setGames(gamesArray);
-
-                /*
-                for (var i = 0; i < gamesArray.length; i++) {
-                    const game = gamesArray[i];
-                    const parameters = game[4];
-                    const map = parameters.i;
-                    let link = url + game[0];
-                    let anchor =
-                        '<a target="_blank" href="' +
-                        link +
-                        '">' +
-                        map +
-                        "</a>";
-                    resultElement.innerHTML +=
-                        "<li>" +
-                        anchor +
-                        " - totPosti: " +
-                        game[3] +
-                        " - liberi: " +
-                        (game[3] - game[2]) +
-                        "</li>";
-                }
-
-                const content: HTMLElement | null =
-                    document.getElementById("vanish");
-                if (content !== null) {
-                    if (gamesArray.length != 0) content.style.display = "flex";
-                    else content.style.display = "none";
-                }
-                */
             })
             .catch((error) => {
                 console.error("Error fetching data:", error);
@@ -122,9 +71,7 @@ const Popup = () => {
                     <div className="row">
                         <div className="col">
                             <div className="row custom-row">
-                                <div
-                                    className="col center-all"
-                                >
+                                <div className="col center-all">
                                     <button
                                         className="custom-button btn btn-primary btn-circle counter-button center-all"
                                         id="request-decrement-counter"
@@ -142,9 +89,7 @@ const Popup = () => {
                                         {availablePlayersCount}
                                     </h3>
                                 </div>
-                                <div
-                                    className="col center-all"
-                                >
+                                <div className="col center-all">
                                     <button
                                         className="custom-button btn btn-primary btn-circle counter-button center-all"
                                         id="request-increment-counter"
@@ -174,18 +119,24 @@ const Popup = () => {
                             <div className="scrollable-div">
                                 {games &&
                                     games.map((game: any) => {
-                                        const parameters = game[4]
-                                        const map = parameters.i
+                                        const parameters = game[4];
+                                        const map = parameters.i;
                                         const gameLink = url + game[0];
 
-                                        return <>
-                                            <li>
-                                                <a target="_blank" href={gameLink} >
-                                                {map}
-                                                </a>
-                                                - totPosti: {game[3]} - liberi: {game[3]-game[2]}
-                                            </li>
-                                        </>;
+                                        return (
+                                            <>
+                                                <li>
+                                                    <a
+                                                        target="_blank"
+                                                        href={gameLink}
+                                                    >
+                                                        {map}
+                                                    </a>
+                                                    - totPosti: {game[3]} -
+                                                    liberi: {game[3] - game[2]}
+                                                </li>
+                                            </>
+                                        );
                                     })}
                             </div>
                         </div>
